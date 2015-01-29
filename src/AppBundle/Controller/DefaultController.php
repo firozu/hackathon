f983f774a2fa2031d141918caf5f8485aed32c00
dacross <dacross@ea.com>
2015-01-29 10:49:33 -0500

    Stub controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig', array('name' => $name));
+        // This is the primary page for the pc browser to hit
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function clientConnectAction(Request $request)
+    {
+        // This is where the mobile client goes to get the page it uses
+        return $this->render('AppBundle:Default:client.html.twig', ['gameId' => $gameId]);
     }
 }
